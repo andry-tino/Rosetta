3c9fa47b48dcb65f6a3fe0680b975db5443a89dc
Andrea <andry.tino@gmail.com>
2015-09-24 21:42:11 +0000

    More initial logic

--- Rosetta/TranslationUnits/compound/ClassDeclarationTranslationUnit.cs
+++ Rosetta/TranslationUnits/compound/ClassDeclarationTranslationUnit.cs
@@ -6,6 +6,7 @@
 namespace Rosetta.Translation
 {
     using System;
+    using System.IO;
     using System.Collections.Generic;
 
     /// <summary>
@@ -13,20 +14,56 @@ namespace Rosetta.Translation
     /// </summary>
     public class ClassDeclarationTranslationUnit : ITranslationUnit, ICompoundTranslationUnit
     {
-        private VisibilityToken visibility;
-        private string name;
-        private string baseClassName;
-        private IEnumerable<string> interfaces;
+        // Inner units
+        private IEnumerable<ITranslationUnit> memberDeclarations;
+        private IEnumerable<ITranslationUnit> constructorDeclarations;
+        private IEnumerable<ITranslationUnit> propertyDeclarations;
+        private IEnumerable<ITranslationUnit> methodDeclarations;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ClassDeclarationTranslationUnit"/> class.
         /// </summary>
-        public ClassDeclarationTranslationUnit()
+        private ClassDeclarationTranslationUnit()
         {
-            this.visibility = VisibilityToken.None;
-            this.name = string.Empty;
-            this.baseClassName = null;
-            this.interfaces = new List<string>();
+            this.Visibility = VisibilityToken.None;
+            this.Name = string.Empty;
+            this.BaseClassName = null;
+            this.Interfaces = new List<string>();
+
+            this.memberDeclarations = new List<ITranslationUnit>();
+            this.constructorDeclarations = new List<ITranslationUnit>();
+            this.propertyDeclarations = new List<ITranslationUnit>();
+            this.methodDeclarations = new List<ITranslationUnit>();
+        }
+
+        private VisibilityToken Visibility { get; set; }
+        private string Name { get; set; }
+        private string BaseClassName { get; set; }
+        private IEnumerable<string> Interfaces { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="visibility"></param>
+        /// <param name="name"></param>
+        /// <param name="baseClassName"></param>
+        /// <returns></returns>
+        public ClassDeclarationTranslationUnit Create(VisibilityToken visibility, string name, string baseClassName)
+        {
+            return new ClassDeclarationTranslationUnit()
+            {
+                Visibility = visibility,
+                Name = name,
+                BaseClassName = baseClassName
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="interfaceName"></param>
+        public void AddImplementedInterfaceName(string interfaceName)
+        {
         }
 
         /// <summary>
@@ -46,17 +83,39 @@ namespace Rosetta.Translation
         /// <returns></returns>
         public string Translate()
         {
-            return string.Format(
-                "{0} class {1} { {0} }"
-                ); // Call other translation units
-        }
+            StringWriter writer = new StringWriter();
 
-        /// <summary>
-        /// Sets the visibility of the class.
-        /// </summary>
-        public VisibilityToken Visibility
-        {
-            set { this.visibility = value; }
+            // Opening declaration
+            writer.WriteLine("{0} class {1} {2}");
+
+            // Translating members first
+            foreach (ITranslationUnit translationUnit in this.memberDeclarations)
+            {
+                // TODO
+            }
+
+            // Then constructors
+            foreach (ITranslationUnit translationUnit in this.constructorDeclarations)
+            {
+                // TODO
+            }
+
+            // Then properties
+            foreach (ITranslationUnit translationUnit in this.propertyDeclarations)
+            {
+                // TODO
+            }
+
+            // Finally methods
+            foreach (ITranslationUnit translationUnit in this.methodDeclarations)
+            {
+                // TODO
+            }
+
+            // Closing
+            writer.WriteLine("{0}", Lexems.CloseCurlyBracket);
+
+            return writer.ToString();
         }
 
         #region Compound translation unit methods
---
+++ Rosetta/TranslationUnits/tokens/Lexems.cs
@@ -0,0 +1,45 @@
+﻿/// <summary>
+/// Lexems.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.Translation
+{
+    using System;
+
+    /// <summary>
+    /// Container for lexems.
+    /// </summary>
+    public static class Lexems
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string OpenCurlyBracket = "{";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string CloseCurlyBracket = "}";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string OpenRoundBracket = "(";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string CloseRoundBracket = ")";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string OpenSquareBracket = "[";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string CloseSquareBracket = "]";
+    }
+}
