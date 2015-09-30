ee11e018b87c8bd3977ac8cda0cee0e1d81c6a5e
Andrea <andry.tino@gmail.com>
2015-09-30 09:08:40 +0200

    More basic logic for test runner

--- test/TestRunner.ps1
+++ test/TestRunner.ps1
@@ -2,16 +2,20 @@
 # This script expects to have defined a few things
 
 param(
-	[parameter(mandatory = $true)] [string]	$MSTestPath,
-	[parameter(mandatory = $true)] [string]	$WorkspacePath,
-	[string[]] $TestAssemblies = @('ASTWalker.UnitTests', 'TranslationUnits.UnitTests')
+  [parameter(mandatory = $true)] [string] $MSTestPath,
+  [parameter(mandatory = $true)] [string] $WorkspacePath,
+  [string[]] $TestAssemblies = @('ASTWalker.UnitTests', 'TranslationUnits.UnitTests')
 );
 
 foreach ($TestAssembly in $TestAssemblies)
 {
-	$PathToTestAssembly = join-path -Path $WorkspacePath -ChildPath $TestAssembly;
-	$PathToTestAssembly = join-path -Path $WorkspacePath -ChildPath "bin/Debug/$TestAssembly.dll";
+  $PathToTestAssembly = join-path -Path $WorkspacePath -ChildPath $TestAssembly;
+  $PathToTestAssembly = join-path -Path $WorkspacePath -ChildPath "bin/Debug/$TestAssembly.dll";
 	
-	# Testing
-	& $MSTestPath "/testcontainer:$PathToTestAssembly"
+  # Testing
+  $Result = & $MSTestPath "/testcontainer:$PathToTestAssembly";
+  if ($Result)
+  {
+    throw;
+  }
 }
