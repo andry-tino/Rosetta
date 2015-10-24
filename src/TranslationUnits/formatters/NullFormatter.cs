341af0107bf8e827f61ca74d0e8663cd7b54d7de
Andrea <andry.tino@gmail.com>
2015-10-24 21:57:56 +0000

    Adding null formatter

---
+++ src/TranslationUnits/formatters/NullFormatter.cs
@@ -0,0 +1,37 @@
+﻿/// <summary>
+/// NullFormatter.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.Translation
+{
+    using System;
+
+    /// <summary>
+    /// Does not format.
+    /// </summary>
+    public class NullFormatter : IFormatter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NullFormatter"/> class.
+        /// </summary>
+        public NullFormatter()
+        {
+        }
+
+        /// <summary>
+        /// Formats a line.
+        /// </summary>
+        /// <param name="line">The line to format.</param>
+        /// <returns>Formatted line.</returns>
+        public string FormatLine(string line)
+        {
+            if (line == null)
+            {
+                throw new ArgumentNullException(nameof(line));
+            }
+
+            return line;
+        }
+    }
+}
