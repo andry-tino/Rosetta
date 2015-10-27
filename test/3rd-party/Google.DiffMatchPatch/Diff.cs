c5822d123bc5925faaa52a28c34b0aeddbcfd7e2
Andrea <andry.tino@gmail.com>
2015-10-27 22:05:14 +0000

    Introducing diffing tool
    Introducing Google's `DiffMatchPatch`. This component will be used for
    rendering tests.

---
+++ test/3rd-party/Google.DiffMatchPatch/Diff.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// Diff.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.ThirdParty.Google.DiffMatchPatch
+{
+    using System;
+
+    using DiffMatchPatch;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Diff
+    {
+    }
+}
