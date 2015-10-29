597662aa5f243df0e798e65c10753f875cc81d76
Andrea <andry.tino@gmail.com>
2015-10-29 22:19:11 +0000

    Introducing full stack rendering tests
    Introducing basic structure for full stack rendering tests.

---
+++ test/renderers/ASTWalker.Renderings/Program.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// Program.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.AST.Renderings
+{
+    using System;
+    using System.Configuration;
+
+    /// <summary>
+    /// Writes all renderings to file.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            // Prioritize argument, if none, use config, if none, use current directory
+            string outputDirectory = args.Length == 1 ?
+                args[0] :
+                (ConfigurationManager.AppSettings["OutputFolderPath"] ?? string.Empty);
+
+            if (outputDirectory == string.Empty)
+            {
+                outputDirectory = Environment.CurrentDirectory;
+            }
+
+            Renderer renderer = new Renderer(outputDirectory);
+
+            // Render all
+            renderer.Render();
+        }
+    }
+}
---
+++ test/renderers/ASTWalker.Renderings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ASTWalker.Renderings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ASTWalker.Renderings")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b8808873-9f30-45b8-acd4-a9daba454756")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
