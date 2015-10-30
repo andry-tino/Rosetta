ae14e0ded95cab1faadef4aba2057d9ad0bfce79
Andrea <andry.tino@gmail.com>
2015-10-30 17:36:25 +0000

    Introducing data project for AST Walker rendering tests

---
+++ test/renderers/ASTWalker.Renderings.Data/SimpleEntities.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// SimpleEntities.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.AST.Renderings.Data
+{
+    using System;
+
+    using Rosetta.Renderings.Utils;
+    using Rosetta.Translation;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SimpleEntities
+    {
+        [RenderingResource("SimpleEmptyClass.ts")]
+        public string RenderSimpleEmptyClass()
+        {
+            // Use ASTWalker
+            // Get TU and run Translate()
+            return "";
+        }
+    }
+}
