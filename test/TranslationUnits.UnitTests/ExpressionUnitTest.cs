47094fbcf6161c64c4b2e8ccdefea76ba789507f
Andrea <andry.tino@gmail.com>
2015-12-10 17:21:05 +0000

    Adding unit tests for expression TUs
    Adding initial unit tests for expressions translation units.

---
+++ test/TranslationUnits.UnitTests/ExpressionUnitTest.cs
@@ -0,0 +1,96 @@
+﻿/// <summary>
+/// ExpressionUnitTest.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.Translation.UnitTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Rosetta.Translation;
+    using Rosetta.Translation.UnitTests.Data;
+    using Utils = Rosetta.Tests.Utils;
+
+    [TestClass]
+    public class ExpressionUnitTest
+    {
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+        }
+
+        [ClassCleanup]
+        public static void CleanUp()
+        {
+        }
+
+        [TestMethod]
+        public void IntegerAddition()
+        {
+            ITranslationUnit number1 = Expressions.RandomIntegerLiteral;
+            ITranslationUnit number2 = Expressions.RandomIntegerLiteral;
+
+            ITranslationUnit translationUnit = BinaryExpressionTranslationUnit.Create(number1, number2, OperatorToken.Addition);
+
+            string typescript = translationUnit.Translate();
+            new Utils.FileWriter(TestSuite.Context).WriteToFile(typescript,
+                string.Format("{0}.Code", nameof(this.IntegerAddition)),
+                Utils.FileType.TypeScript);
+
+            Assert.AreEqual(string.Format("{0} + {1}", number1.Translate(), number2.Translate()), 
+                typescript, "Expression does not match expected!");
+        }
+
+        [TestMethod]
+        public void IntegerMultiply()
+        {
+            ITranslationUnit number1 = Expressions.RandomIntegerLiteral;
+            ITranslationUnit number2 = Expressions.RandomIntegerLiteral;
+
+            ITranslationUnit translationUnit = BinaryExpressionTranslationUnit.Create(number1, number2, OperatorToken.Multiplication);
+
+            string typescript = translationUnit.Translate();
+            new Utils.FileWriter(TestSuite.Context).WriteToFile(typescript,
+                string.Format("{0}.Code", nameof(this.IntegerAddition)),
+                Utils.FileType.TypeScript);
+
+            Assert.AreEqual(string.Format("{0} * {1}", number1.Translate(), number2.Translate()),
+                typescript, "Expression does not match expected!");
+        }
+
+        [TestMethod]
+        public void IntegerSubtract()
+        {
+            ITranslationUnit number1 = Expressions.RandomIntegerLiteral;
+            ITranslationUnit number2 = Expressions.RandomIntegerLiteral;
+
+            ITranslationUnit translationUnit = BinaryExpressionTranslationUnit.Create(number1, number2, OperatorToken.Subtraction);
+
+            string typescript = translationUnit.Translate();
+            new Utils.FileWriter(TestSuite.Context).WriteToFile(typescript,
+                string.Format("{0}.Code", nameof(this.IntegerAddition)),
+                Utils.FileType.TypeScript);
+
+            Assert.AreEqual(string.Format("{0} - {1}", number1.Translate(), number2.Translate()),
+                typescript, "Expression does not match expected!");
+        }
+
+        [TestMethod]
+        public void IntegerDivide()
+        {
+            ITranslationUnit number1 = Expressions.RandomIntegerLiteral;
+            ITranslationUnit number2 = Expressions.RandomIntegerLiteral;
+
+            ITranslationUnit translationUnit = BinaryExpressionTranslationUnit.Create(number1, number2, OperatorToken.Divide);
+
+            string typescript = translationUnit.Translate();
+            new Utils.FileWriter(TestSuite.Context).WriteToFile(typescript,
+                string.Format("{0}.Code", nameof(this.IntegerAddition)),
+                Utils.FileType.TypeScript);
+
+            Assert.AreEqual(string.Format("{0} / {1}", number1.Translate(), number2.Translate()),
+                typescript, "Expression does not match expected!");
+        }
+    }
+}
---
+++ test/TranslationUnits.UnitTests/data/Expressions.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Expressions.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.Translation.UnitTests.Data
+{
+    using System;
+
+    using Rosetta.Translation;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class Expressions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static ITranslationUnit RandomIntegerLiteral
+        {
+            get { return LiteralTranslationUnit<int>.Create(RandomInteger); }
+        }
+        
+        private static int RandomInteger
+        {
+            get { return new Random(0).Next(0, 1000); }
+        }
+    }
+}
