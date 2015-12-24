1c934f8cbc22af065fd699a577bea6822bfac9b1
Andrea <andry.tino@gmail.com>
2015-12-24 17:16:32 +0000

    Considered test for groups of statements

---
+++ test/renderers/TranslationUnits.Renderings.Data/StatementsGroups.cs
@@ -0,0 +1,29 @@
+﻿/// <summary>
+/// StatementsGroups.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.Translation.Renderings.Data
+{
+    using System;
+
+    using Rosetta.Renderings.Utils;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StatementsGroups
+    {
+        /// <summary>
+        /// TODO: Implement
+        /// </summary>
+        /// <returns></returns>
+        [RenderingResource("SimpleGroup.ts")]
+        public string RenderSimpleGroup()
+        {
+            ITranslationUnit translationUnit = null;
+
+            return translationUnit.Translate();
+        }
+    }
+}
