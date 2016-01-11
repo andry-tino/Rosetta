d9e58bd1387653579af2b3d3b5d93d54cf37f2bc
Andrea <andry.tino@gmail.com>
2016-01-11 21:24:11 +0000

    Introduced visibility converter
    Placeholder, logic to be added.

---
+++ src/ASTWalker/cs-ts-converters/VisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// VisibilityConverter.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.AST.Converters
+{
+    using System;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    /// <summary>
+    /// Converter for properly converting C# visibilities into TypeScript visibilities.
+    /// </summary>
+    internal class VisibilityConverter
+    {
+        
+    }
+}
