d2c8149c1675f9fd5ed266bf4d687641597b6706
Andrea <andry.tino@gmail.com>
2016-01-26 17:09:37 +0000

    More tests for interfaces
    Expanding coverage in AST tests for interfaces.

--- test/ASTWalker.UnitTests/InterfaceTest.cs
+++ test/ASTWalker.UnitTests/InterfaceTest.cs
@@ -70,5 +70,147 @@ namespace Rosetta.AST.UnitTests
             Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(0), typeof(MethodSignatureDeclarationTranslationUnit));
             Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(1), typeof(MethodSignatureDeclarationTranslationUnit));
         }
+
+        [TestMethod]
+        public void Properties()
+        {
+            string source = @"
+                public interface MyInterface {
+                    int Property1 { get; set; }
+                    string Property2 { get; set; }
+                }
+            ";
+
+            // Getting the AST node
+            CSharpSyntaxTree tree = ASTExtractor.Extract(source);
+            Source.ProgramRoot = tree;
+
+            SyntaxNode node = new NodeLocator(tree).LocateFirst(typeof(InterfaceDeclarationSyntax));
+            InterfaceDeclarationSyntax interfaceDeclarationNode = node as InterfaceDeclarationSyntax;
+
+            // Creating the walker
+            var astWalker = MockedInterfaceASTWalker.Create(interfaceDeclarationNode);
+
+            // Getting the translation unit
+            astWalker.Walk();
+
+            // Checking
+            Assert.IsNotNull(astWalker.InterfaceDeclaration);
+
+            // Checking signatures
+            Assert.IsNotNull(astWalker.InterfaceDeclaration.Signatures);
+            Assert.IsTrue(astWalker.InterfaceDeclaration.Signatures.Count() > 0);
+            Assert.AreEqual(2, astWalker.InterfaceDeclaration.Signatures.Count());
+
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(0), typeof(MethodSignatureDeclarationTranslationUnit));
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(1), typeof(MethodSignatureDeclarationTranslationUnit));
+        }
+
+        [TestMethod]
+        public void GetOnlyProperties()
+        {
+            string source = @"
+                public interface MyInterface {
+                    int Property1 { get; }
+                    string Property2 { get; }
+                }
+            ";
+
+            // Getting the AST node
+            CSharpSyntaxTree tree = ASTExtractor.Extract(source);
+            Source.ProgramRoot = tree;
+
+            SyntaxNode node = new NodeLocator(tree).LocateFirst(typeof(InterfaceDeclarationSyntax));
+            InterfaceDeclarationSyntax interfaceDeclarationNode = node as InterfaceDeclarationSyntax;
+
+            // Creating the walker
+            var astWalker = MockedInterfaceASTWalker.Create(interfaceDeclarationNode);
+
+            // Getting the translation unit
+            astWalker.Walk();
+
+            // Checking
+            Assert.IsNotNull(astWalker.InterfaceDeclaration);
+
+            // Checking signatures
+            Assert.IsNotNull(astWalker.InterfaceDeclaration.Signatures);
+            Assert.IsTrue(astWalker.InterfaceDeclaration.Signatures.Count() > 0);
+            Assert.AreEqual(2, astWalker.InterfaceDeclaration.Signatures.Count());
+
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(0), typeof(MethodSignatureDeclarationTranslationUnit));
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(1), typeof(MethodSignatureDeclarationTranslationUnit));
+        }
+
+        [TestMethod]
+        public void SetOnlyProperties()
+        {
+            string source = @"
+                public interface MyInterface {
+                    int Property1 { set; }
+                    string Property2 { set; }
+                }
+            ";
+
+            // Getting the AST node
+            CSharpSyntaxTree tree = ASTExtractor.Extract(source);
+            Source.ProgramRoot = tree;
+
+            SyntaxNode node = new NodeLocator(tree).LocateFirst(typeof(InterfaceDeclarationSyntax));
+            InterfaceDeclarationSyntax interfaceDeclarationNode = node as InterfaceDeclarationSyntax;
+
+            // Creating the walker
+            var astWalker = MockedInterfaceASTWalker.Create(interfaceDeclarationNode);
+
+            // Getting the translation unit
+            astWalker.Walk();
+
+            // Checking
+            Assert.IsNotNull(astWalker.InterfaceDeclaration);
+
+            // Checking signatures
+            Assert.IsNotNull(astWalker.InterfaceDeclaration.Signatures);
+            Assert.IsTrue(astWalker.InterfaceDeclaration.Signatures.Count() > 0);
+            Assert.AreEqual(2, astWalker.InterfaceDeclaration.Signatures.Count());
+
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(0), typeof(MethodSignatureDeclarationTranslationUnit));
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(1), typeof(MethodSignatureDeclarationTranslationUnit));
+        }
+
+        [TestMethod]
+        public void MixedProperties()
+        {
+            string source = @"
+                public interface MyInterface {
+                    int Property1 { get; }
+                    string Property2 { set; }
+                    bool Property3 { get; set; }
+                }
+            ";
+
+            // Getting the AST node
+            CSharpSyntaxTree tree = ASTExtractor.Extract(source);
+            Source.ProgramRoot = tree;
+
+            SyntaxNode node = new NodeLocator(tree).LocateFirst(typeof(InterfaceDeclarationSyntax));
+            InterfaceDeclarationSyntax interfaceDeclarationNode = node as InterfaceDeclarationSyntax;
+
+            // Creating the walker
+            var astWalker = MockedInterfaceASTWalker.Create(interfaceDeclarationNode);
+
+            // Getting the translation unit
+            astWalker.Walk();
+
+            // Checking
+            Assert.IsNotNull(astWalker.InterfaceDeclaration);
+
+            // Checking signatures
+            Assert.IsNotNull(astWalker.InterfaceDeclaration.Signatures);
+            Assert.IsTrue(astWalker.InterfaceDeclaration.Signatures.Count() > 0);
+            Assert.AreEqual(3, astWalker.InterfaceDeclaration.Signatures.Count());
+
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(0), typeof(MethodSignatureDeclarationTranslationUnit));
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(1), typeof(MethodSignatureDeclarationTranslationUnit));
+            Assert.IsInstanceOfType(astWalker.InterfaceDeclaration.Signatures.ElementAt(2), typeof(MethodSignatureDeclarationTranslationUnit));
+        }
     }
 }
