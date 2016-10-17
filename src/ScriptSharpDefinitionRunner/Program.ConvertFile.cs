302d51142ee7ceeabd0c9ff7ddd7a096f7df88cf
Andrea <andry.tino@gmail.com>
2016-10-17 15:26:12 +0200

    Introducing ScriptSharpDefinitionRunner
    Introducing runner for ScriptSharp definitions. Parts of the project's
    code are disabled, waiting for AST walkers in ScriptSharpDefinition to
    be completed.

---
+++ src/ScriptSharpDefinitionRunner/Program.ConvertFile.cs
@@ -0,0 +1,111 @@
+﻿/// <summary>
+/// Program.ConvertFile.cs
+/// Andrea Tino - 2015
+/// </summary>
+
+namespace Rosetta.ScriptSharp.Definition.Runner
+{
+    using System;
+    using System.IO;
+    
+    using Rosetta.Executable;
+
+    /// <summary>
+    /// Part of program responsible for translating one single file.
+    /// </summary>
+    internal partial class Program
+    {
+        protected virtual void ConvertFile()
+        {
+            this.InitializeForFileConversion();
+            this.PrepareFiles();
+            this.EmitFiles();
+        }
+
+        // TODO: Move this in shared part as also the routine for converting projects will need to do this
+        protected virtual void InitializeForFileConversion()
+        {
+            // Making sure we translate it into absolute path
+            // Attention: Path correctness is checked later
+            // Important: File path must be converted into absolute now 
+            // as the calculation for output folder relies on this!
+            this.filePath = this.GetFilePath(this.filePath);
+
+            // Setting output folder
+            // Making sure this gets translated into absolute path
+            this.outputFolder = this.GetOutputFolderForFile(this.outputFolder);
+
+            // Initializing the file manager
+            this.fileManager = new FileManager(this.outputFolder);
+            this.fileManager.FileConversionProvider = PerformConversion;
+        }
+        
+        protected virtual void PrepareFiles()
+        {
+            // This will perform anothe file existing check, redundant as we 
+            // do it in initialization routine, but fine!
+            fileManager.AddFile(this.filePath, this.fileName);
+        }
+        
+        protected virtual void EmitFiles()
+        {
+            var writtenFiles = fileManager.WriteAllFilesToDestination();
+
+            foreach (var file in writtenFiles)
+            {
+                Console.WriteLine("Wrote file {0}", file);
+            }
+        }
+
+        #region Helpers
+
+        /// <summary>
+        /// We proceed in this order:
+        /// 1. If no path specified: take the path of the input file.
+        /// 2. Otherwise, a path is specified: use that.
+        /// </summary>
+        /// <param name="userInput">The user input. Can be <code>null</code>.</param>
+        /// <returns>The absolute path to the output folder basing on user input.</returns>
+        private string GetOutputFolderForFile(string userInput)
+        {
+            if (userInput != null)
+            {
+                // User provided a path: check the path is all right
+                // But since the path might be relative, we must ensure it is absolute
+                string userProvidedPath = FileManager.GetAbsolutePath(userInput);
+                if (FileManager.IsDirectoryPathCorrect(userProvidedPath))
+                {
+                    return userProvidedPath;
+                }
+
+                // Wrong path
+                throw new InvalidOperationException("Invalid path provided for output folder!", 
+                    new DirectoryNotFoundException(string.Format("Directory {0} not found", userInput)));
+            }
+
+            // User did not provide a path, we get the path of the input file
+            // Attention, the path might be relative
+            if (FileManager.IsDirectoryWhereFileResidesCorrect(this.filePath))
+            {
+                return FileManager.ExtractDirectoryPathFromFilePath(this.filePath);
+            }
+
+            throw new InvalidOperationException("Invalid path provided for input file for extracting output directory!",
+                new FileNotFoundException("File not found", filePath));
+        }
+
+        /// <summary>
+        /// Checks that the path is OK and also translates into absolute path.
+        /// </summary>
+        /// <param name="userInput">
+        /// The user input. At this point this is supposed not to be <code>null</code>.
+        /// </param>
+        /// <returns></returns>
+        private string GetFilePath(string userInput)
+        {
+            return FileManager.GetAbsolutePath(userInput);
+        }
+
+        #endregion
+    }
+}
---
+++ src/ScriptSharpDefinitionRunner/Program.cs
@@ -0,0 +1,246 @@
+﻿/// <summary>
+/// Program.cs
+/// Andrea Tino - 2016
+/// </summary>
+
+namespace Rosetta.ScriptSharp.Definition.Runner
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Rosetta.Executable;
+    using Rosetta.Executable.Exceptions;
+
+    using Mono.Options;
+
+    /// <summary>
+    /// Main program.
+    /// </summary>
+    /// <remarks>
+    /// Members protected for testability.
+    /// </remarks>
+    internal partial class Program
+    {
+        protected static Program instance;
+
+        protected string[] args;
+        protected OptionSet options;
+
+        protected string filePath = null;         // File to convert
+        protected string outputFolder = null;     // The output folder path for destination files
+        protected string fileName = null;         // The output file name
+        protected bool verbose = false;           // Verbosity
+        protected bool help = false;              // Show help message
+
+        protected FileManager fileManager;
+
+        public const string UnnamedArgumentName = "unnamed";
+
+        public const string FileArgumentName = "file";
+        public const string FileArgumentChar = "f";
+        public const string OutputArgumentName = "output";
+        public const string OutputArgumentChar = "o";
+        public const string FileNameArgumentName = "filename";
+        public const string FileNameArgumentChar = "n";
+        public const string VerboseArgumentName = "verbose";
+        public const string VerboseArgumentChar = "v";
+        public const string HelpArgumentName = "help";
+        public const string HelpArgumentChar = "h";
+
+        public string FileOption
+        {
+            get { return string.Format("{0}|{1}=", FileArgumentName, FileArgumentChar); }
+        }
+
+        public string OutputOption
+        {
+            get { return string.Format("{0}|{1}=", OutputArgumentName, OutputArgumentChar); }
+        }
+
+        public string FileNameOption
+        {
+            get { return string.Format("{0}|{1}=", FileNameArgumentName, FileNameArgumentChar); }
+        }
+
+        public string VerboseOption
+        {
+            get { return string.Format("{0}|{1}", VerboseArgumentName, VerboseArgumentChar); }
+        }
+
+        public string HelpOption
+        {
+            get { return string.Format("{0}|{1}", HelpArgumentName, HelpArgumentChar); }
+        }
+
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            instance = new Program(args);
+            instance.Run();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Program"/> class.
+        /// </summary>
+        /// <param name="args"></param>
+        public Program(string[] args)
+        {
+            this.args = args;
+            this.options = new OptionSet()
+            {
+                { FileOption, "The path to the C# {FILE} to convert into TypeScript.",
+                  value => this.filePath = value },
+                { OutputOption, "The {OUTPUT} folder path where Rosetta will emit all output files.",
+                  value => this.outputFolder = value },
+                { FileNameOption, "The {FILENAME} to use for output file. Valid only when {FILE} is specified.",
+                  value => this.fileName = value },
+                { VerboseOption, "Increase debug message {VERBOSE} level.",
+                  value => this.verbose = value != null },
+                { HelpOption,  "Show this message and exit.",
+                  value => this.help = value != null },
+            };
+        }
+
+        /// <summary>
+        /// Starts the program.
+        /// </summary>
+        public void Run()
+        {
+            List<string> extra;
+            try
+            {
+                extra = this.options.Parse(this.args);
+                this.HandleExtraParameters(extra);
+            }
+            catch (OptionException e)
+            {
+                this.HandleOptionException(e);
+                return;
+            }
+
+            // If user provided no input arguments, show help
+            if (this.args.Length == 0)
+            {
+                Console.Write("No input provided!");
+                this.ShowHelp();
+
+                return;
+            }
+
+            this.RunCore();
+        }
+
+        /// <summary>
+        /// Runs the main logic.
+        /// </summary>
+        protected virtual void RunCore()
+        {
+            // Priority to help
+            if (help)
+            {
+                this.ShowHelp();
+                return;
+            }
+
+            try
+            {
+                // We start by considering whether the user specified a file to convert
+                if (this.filePath != null)
+                {
+                    this.ConvertFile();
+                    return;
+                }
+
+                // If we get to here, then basically nothing happens, the user needs to specify options
+                this.HandleNoFeasibleExecution();
+            }
+            catch (Exception e)
+            {
+                this.HandleError(e);
+                return;
+            }
+        }
+
+        protected virtual void HandleError(Exception e)
+        {
+            Console.WriteLine("An error occurred!");
+
+#if DEBUG
+            Console.WriteLine("Error details below:");
+            Console.WriteLine("An error occurred: {0}!", e.Message);
+            Console.WriteLine(e.StackTrace);
+#endif
+        }
+
+        protected virtual void HandleOptionException(OptionException e)
+        {
+            Console.Write("An error occurred while reading input: ");
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Try using option `--help' for more information.");
+        }
+
+        protected virtual void HandleNoFeasibleExecution()
+        {
+            Console.WriteLine("A file or a project to convert should be specified!");
+            this.ShowHelp();
+        }
+
+        protected virtual void HandleExtraParameters(IEnumerable<string> extra)
+        {
+            int count = extra.Count();
+
+            if (count == 0)
+            {
+                return;
+            }
+
+            // An extra parameter is allowed: implicit [--file]
+            // However if more unhandled parameters are found, this is an error
+            if (count > 1)
+            {
+                throw new OptionException("Cannot handle more than one unnamed parameter!",
+                    "Default parameters", new DefaultOptionException(extra.ToArray()));
+            }
+
+            // Also, if parameter --file has been specified together with one unnamed 
+            // parameter, we will throw an exception indicatring the conflict
+            if (this.filePath != null)
+            {
+                throw new OptionException("Conflict occurred when processing input: " +
+                    "an unnamed parameter and a file-path parameter have been both specified.",
+                    FileArgumentName, new ConflictingOptionsException(UnnamedArgumentName, FileArgumentName));
+            }
+
+            // If everything is fine, just apply to file-path
+            this.filePath = extra.ElementAt(0);
+        }
+
+        private static string PerformConversion(string source)
+        {
+            // TODO: Enable once walkers are ready
+
+            //var program = new ProgramWrapper(source);
+
+            //return program.Output;
+
+            return "";
+        }
+
+        #region Helpers
+
+        protected virtual void ShowHelp()
+        {
+            Console.WriteLine("Usage: RosettaScriptSharpDefinition [OPTIONS]+ message");
+            Console.WriteLine("Generates TypeScript definitino files from C# files.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            this.options.WriteOptionDescriptions(Console.Out);
+        }
+
+        #endregion
+    }
+}
